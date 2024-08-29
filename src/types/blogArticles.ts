1a575ebe4f917b703a21e0a17fcf8854591e3d2b
Daniel Cabral <danicabral15@outlook.com>
2024-08-29 13:03:16 -0300

    refactor: Update Card component to include product name and price

---
+++ src/types/blogArticles.ts
@@ -0,0 +1,10 @@
+export interface Frontmatter {
+  layout: string;
+  title: string;
+  author: string;
+  date: string;
+  tags: string[];
+  short_description: string;
+  category: string;
+  cover: string;
+}
